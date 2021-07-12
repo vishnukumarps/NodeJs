00f20d509104d7cff4dff4bfde8b450e23b3e47f
vishnu <vishnu.kumarps@claysys.net>
2021-07-12 14:36:41 +0530

    test

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = new express.Router();
-const auth= require('../middleware/auth');
+const auth = require('../middleware/auth');
 
-var tasks=[{"name":"Task1", "description":"description1"}];
+var tasks = [{ "name": "Task1", "description": "description1" }];
 
 
-router.post('/task/add',async (req,res)=>{
+router.post('/task/add', async (req, res) => {
    tasks.push(req.body);
    res.send(tasks);
 });
 
-router.get('/task/:id',auth,async (req,res)=>{
-    var task=tasks.find(x=>x.name===req.params.id);
-    res.send(task);
- });
- 
+router.get('/task/:id', auth, async (req, res) => {
+   var task = tasks.find(x => x.name === req.params.id);
+   res.send(task);
+});
+
 module.exports = router;
